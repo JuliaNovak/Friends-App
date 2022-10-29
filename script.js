ba767f9fc172c0ab57427c4f85e409034e1d5c1f
JuliaNovak <julia.novak9@gmail.com>
2022-10-29 21:02:57 +0200

    implement pagination

--- script.js
+++ script.js
@@ -2,33 +2,60 @@ const friendsList = document.querySelector(".friends-list");
 const friendCard = document.createElement("div");
 friendCard.classList.add(".friend-card");
 let usersData = [];
-const url = "https://randomuser.me/api/?results=30";
+const url = "https://randomuser.me/api/?results=100";
+let optionsForFilter = {};
+let filteredUsers = [];
+const goBackBtn = document.querySelector(".go-back-btn");
+const goFurtherBtn = document.querySelector(".go-further-btn");
+let usersToShow = 20;
+let page = 0;
 
 async function getData() {
+   goBackBtn.style.visibility = "hidden";
+   goFurtherBtn.style.visibility = "hidden";
    const response = await fetch(url);
    const data = await response.json();
    usersData = data.results;
-   render(data.results);
+   filteredUsers = usersData;
+   render(usersData);
+   goBackBtn.style.visibility = "visible";
+   goFurtherBtn.style.visibility = "visible";
 }
 getData();
 
-function render(usersArray) {
-   friendsList.innerHTML = "";
-   usersArray.slice.map((user) => {
-      friendsList.innerHTML += `<div class='friend-card'>
-    <img class='friend-avatar' src='${user.picture.large}'/>
-    <p class='friends-info name'>${user.name.first} ${user.name.last}</p>
-    <p class='friends-info'>${user.gender}, ${user.dob.age}y.o.</p>
-    <p class='friends-info'>${user.phone}</p>
-    <p class='friends-info'>${user.email}</p>
-    </div>`;
-   });
+function paginate(direction) {
+   optionsForFilter.page = page;
+   if (direction === "back") {
+      page -= 1;
+   } else if (direction === "further") {
+      page += 1;
+   }
+   render(filteredUsers);
 }
+goBackBtn.addEventListener("click", () => paginate("back"));
+goFurtherBtn.addEventListener("click", () => paginate("further"));
 
-let optionsForFilter = {};
+function render(usersArray) {
+   window.scrollTo(0, 0);
+   goBackBtn.disabled = page <= 0;
+   goFurtherBtn.disabled = page >= filteredUsers.length / usersToShow - 1;
 
+   friendsList.innerHTML = "";
+   usersArray
+      .slice(page * usersToShow, page * usersToShow + usersToShow)
+      .map((user) => {
+         friendsList.innerHTML += `<div class='friend-card'>
+      <img class='friend-avatar' src='${user.picture.large}'/>
+      <p class='friends-info name'>${user.name.first} ${user.name.last}</p>
+      <p class='friends-info'>${user.gender}, ${user.dob.age}y.o.</p>
+      <p class='friends-info'>${user.phone}</p>
+      <p class='friends-info'>${user.email}</p>
+      </div>`;
+      });
+}
 const searchInput = document.querySelector(".search-input");
 function filterByInput() {
+   page = 0;
    optionsForFilter.name = searchInput.value.toLowerCase();
    prepareUsersToRender();
 }
@@ -45,13 +72,12 @@ function onGenderSelected() {
 }
 
 function prepareUsersToRender() {
-   render(sortUsers(filterUsers()));
+   render(sortUsers(filterUsers(usersData)));
 }
 
-function filterUsers() {
-   let filteredUsers = usersData;
+function filterUsers(usersCollection) {
    if (optionsForFilter.name) {
-      filteredUsers = filteredUsers.filter((user) => {
+      filteredUsers = usersCollection.filter((user) => {
          let userName = `${user.name.first} ${user.name.last}`
             .toLowerCase()
             .includes(optionsForFilter.name);
@@ -59,7 +85,7 @@ function filterUsers() {
       });
    }
    if (optionsForFilter.gender) {
-      filteredUsers = filteredUsers.filter((user) => {
+      filteredUsers = usersCollection.filter((user) => {
          const userGender = optionsForFilter.gender;
          if (user.gender === userGender || userGender === "all") {
             return user;
@@ -69,16 +95,16 @@ function filterUsers() {
    return filteredUsers;
 }
 
-function sortUsers(filteredUsers) {
+function sortUsers(filteredUsersLocal) {
    if (optionsForFilter.sort && optionsForFilter.sort.includes("sortAge")) {
-      filteredUsers = filteredUsers.sort((a, b) => {
+      filteredUsers = filteredUsersLocal.sort((a, b) => {
          [a, b] =
             optionsForFilter.sort === "sortAge-descending" ? [b, a] : [a, b];
          return a.dob.age - b.dob.age;
       });
    }
    if (optionsForFilter.sort && optionsForFilter.sort.includes("sortAlph")) {
-      filteredUsers = filteredUsers.sort((a, b) => {
+      filteredUsers = filteredUsersLocal.sort((a, b) => {
          [a, b] =
             optionsForFilter.sort === "sortAlph-descending" ? [b, a] : [a, b];
          return a.name.first.localeCompare(b.name.first);
